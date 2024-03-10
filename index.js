574ebb56f6d45fccbb856fcd841fbb820bdf9fd6
zzD3chzz <thcdzch@gmail.com>
2024-03-10 18:48:18 +0700

    update fineltes

---
+++ index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sv = express();
+
+sv.use(express.json());
+sv.use(morgan("combined"));
+
+// sv.use("/v1/movie", movieRoute);
+
+
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() =>
+    sv.listen(process.env.PORT, () =>
+      console.log(
+        `server port http://localhost:${process.env.PORT} is running !!!`
+      )
+    )
+  );
